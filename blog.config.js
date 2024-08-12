e820dd4a63f24cf2d2f34e2df5054404271e2f00
hunter <50197260+Hunter-Fang@users.noreply.github.com>
2024-08-12 21:23:58 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  title: '红桃的烂活儿',
+  author: 'hunterfang',
+  email: '',
+  link: 'https://huntefang.cn',
+  description: '大部分是烂活儿，偶尔写点好的.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
